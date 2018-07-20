e76fc35ccebffc8852461fb0370f1df7e219cf16
xhw <3295468820@qq.com>
2018-07-20 10:15:26 +0800

    最终版本的爬虫程序已经出来了，剩下就是添加URL，邮箱，主题这些了

--- python/scapy_weather.py
+++ python/scapy_weather.py
@@ -2,6 +2,7 @@
 import requests
 import bs4
 import pprint
+import re
 from twilio.rest import Client
 import smtplib
 from email.mime.text import MIMEText
@@ -19,11 +20,23 @@ class getWeather():
         self.soup = bs4.BeautifulSoup(self.response.text,'html.parser')
         self.res = self.get_weather_seven()
         self.life = self.get_life_num()
-        self.body = "\n".join(self.res) + "     " + "\n".join(self.life)
+        self.body = "\n".join(self.res) + "     " + "\n" + "\n".join(self.life)+ "\n"+"\n".join(self.get_concre_data())
+        pprint.pprint(self.body)
+
+    def get_concre_data(self):
+        concre_data_time = self.soup.select("script")
+        #print(concre_data_time[2].string[15:-1])
+        d = json.loads(concre_data_time[2].string[15:-1])
+        #pprint.pprint(d["7d"][:3])
+        res = ["未来三天内详细天气信息",]
+        for i in d["7d"][:3]:
+            for j in i:
+                res.append(re.sub(r',','    ',j[:6]+j[10:-2]))
+        return res
     
     def get_weather_seven(self):#获取七天内天气
         list1 = self.soup.select('ul[class="t clearfix"] > li')
-        res_weather = []
+        res_weather = ["未来七天内天气信息",]
         for i in list1:
             weather = ""
             tem_i = i.select("p[class='tem'] > i")[0].string
@@ -49,7 +62,7 @@ class getWeather():
     
     def get_life_num(self):   #获取当天天气的健康提示信息
         life_num = self.soup.select("ul[class='clearfix'] > li")
-        res_life = []
+        res_life = ["今天健康提示",]
         res_life.append(life_num[0].select("em")[0].string+":"+life_num[0].select("span")[0].string+" "+life_num[0].select("p")[0].string)
         res_life.append(life_num[1].select("p")[0].string)
         res_life.append(life_num[2].select("em")[0].string+":"+life_num[2].select("span")[0].string+" "+life_num[2].select("p")[0].string)
@@ -68,6 +81,8 @@ class send_email_of_weather():
         self.psd = 'nlropspeooyvdaii'       # 发件人邮箱密码(当时申请smtp给的口令)
         if receiver is not None:
             for i in receiver:
+                if i == '1602983878@qq.com':
+                    subject = "给最最最可爱的姝姝"
                 ret = self.mail(i,subject,body)
                 if ret:
                     print("邮件发送%s成功"%i)
@@ -98,12 +113,10 @@ class send_email_of_weather():
 
 if __name__ == '__main__':
     #url顺序是姝姝南宁天气和博白天气，南宁天气，哈尔滨天气
-    url = ["http://www.weather.com.cn/weather/101300101.shtml","http://www.weather.com.cn/weather/101300902.shtml","http://www.weather.com.cn/weather/101300101.shtml","http://www.weather.com.cn/weather/101050101.shtml"]
-    receiver = [['1602983878@qq.com' ],["2311077029@qq.com","1423237806@qq.com","2019607084@qq.com"],["1194157042@qq.com"],["3295468820@qq.com"]]
-    subject = ["给最最最可爱的姝姝","博白天气","南宁天气","哈尔滨天气"]
-    my_dict = {"给最最最可爱的姝姝":['1602983878@qq.com'] ,"博白天气":["2311077029@qq.com","2311077029@qq.com"],"南宁天气":["1194157042@qq.com"]}
-    #for key,value in my_dict.items():
-    #    
+    url = ["http://www.weather.com.cn/weather/101300902.shtml","http://www.weather.com.cn/weather/101300101.shtml","http://www.weather.com.cn/weather/101050101.shtml"]
+    receiver = [["2311077029@qq.com","1423237806@qq.com","2019607084@qq.com"],["1194157042@qq.com",'1602983878@qq.com'],["3295468820@qq.com"]]#'1602983878@qq.com'
+    subject = ["博白天气","南宁天气","哈尔滨天气"]
+    #my_dict = {"给最最最可爱的姝姝":['1602983878@qq.com'] ,"博白天气":["2311077029@qq.com","2311077029@qq.com"],"南宁天气":["1194157042@qq.com"]}
     j = 0
     for i in url:
         weather = getWeather(i)
